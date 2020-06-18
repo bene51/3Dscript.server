000e45553692b1d7ffc81626f64bf705f3237a42
Benjamin Schmid <bene.schmid@gmail.com>
2020-06-18 10:10:06 +0200

    Fix ResizedVirtualStack
    Fix an issue with calculating stack indices.

--- src/main/java/animation3d/server/ResizedVirtualStack.java
+++ src/main/java/animation3d/server/ResizedVirtualStack.java
@@ -32,6 +32,7 @@ public class ResizedVirtualStack extends ImageStack {
 	/** The number of slices in the resized stack */
 	private final int N;
 
+	private final int nT;
 
 
 	public ResizedVirtualStack(int targetWidth,
@@ -48,6 +49,7 @@ public class ResizedVirtualStack extends ImageStack {
 		this.nChannelsOriginal = nChannelsOriginal;
 		this.original = original;
 		int nTnC = original.getSize() / nPlanesOriginal;
+		this.nT = nTnC / nChannelsOriginal;
 		this.nTargetPlanes = (int)Math.ceil((double)nPlanesOriginal / nTargetPlanesPerZ);
 		this.N = nTnC * nTargetPlanes;
 	}
@@ -105,16 +107,25 @@ public class ResizedVirtualStack extends ImageStack {
 		if( n < 1 || n > N)
 			throw new IllegalArgumentException("Argument out of range: "+n);
 
+		// dim[0] = w, dim[1] = h, dim[2] = nC, dim[3] = d, dim[4] = nT
+		// ((n-1)%dim[2])+1;
+		int channel = ((n-1) % nChannelsOriginal) + 1;
+		// (((n-1)/dim[2])%dim[3])+1;
 		int plane = (((n - 1) / nChannelsOriginal) % nTargetPlanes) + 1;
-		int originalPlane = (plane - 1) * nTargetPlanesPerZ + 1;
+		// (((n-1)/(dim[2]*dim[3]))%dim[4])+1;
+		int frame = (((n - 1) / (nChannelsOriginal * nTargetPlanes)) % nT) + 1;
 
-		ImageProcessor ip = original.getProcessor(originalPlane);
+		// (frame-1)*nChannels*nSlices + (slice-1)*nChannels + channel;
+		int originalPlane = (plane - 1) * nTargetPlanesPerZ + 1;
+		int originalIndex = (frame - 1) * nChannelsOriginal * nPlanesOriginal + (originalPlane - 1) * nChannelsOriginal + channel;
+
+		ImageProcessor ip = original.getProcessor(originalIndex);
 		ImageProcessor averaged = ip.convertToFloat();
 		int nToAverage = 1;
 		for(int i = 1; i < nTargetPlanesPerZ; i++) {
-			if(originalPlane + i > nPlanesOriginal)
+			int itmp = originalIndex + i * nChannelsOriginal;
+			if(itmp > nPlanesOriginal)
 				break;
-			int itmp = originalPlane + i * nChannelsOriginal;
 			ImageProcessor tmp = original.getProcessor(itmp);
 			averaged.copyBits(tmp, 0, 0, Blitter.ADD);
 			nToAverage++;
