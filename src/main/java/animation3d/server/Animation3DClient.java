ed68c0467c6d7981c11ad3de54445a784c31557f
Benjamin Schmid <bene.schmid@gmail.com>
2020-07-21 13:04:28 +0200

    Remove some old code.

--- src/main/java/animation3d/server/Animation3DClient.java
+++ src/main/java/animation3d/server/Animation3DClient.java
@@ -10,27 +10,16 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 import java.util.Base64;
 
 import animation3d.renderer3d.Progress;
-import fiji.util.gui.GenericDialogPlus;
-import ij.IJ;
-import ij.Prefs;
-import ij.plugin.AVI_Reader;
-import ij.plugin.PlugIn;
 import omero.gateway.Gateway;
 import omero.gateway.LoginCredentials;
 import omero.gateway.exception.DSOutOfServiceException;
 import omero.gateway.model.ExperimenterData;
 import omero.log.SimpleLogger;
 
-public class Animation3DClient implements PlugIn {
-
-	public static void main(String[] args) {
-		new ij.ImageJ();
-		new Animation3DClient().run(null);
-	}
+public class Animation3DClient {
 
 	public static String omeroLogin(String hostname, int port, String userName, String password) {
 		long start = System.currentTimeMillis();
@@ -58,17 +47,6 @@ public class Animation3DClient implements PlugIn {
         long end = System.currentTimeMillis();
         System.out.println("omero login took "  + (end - start) + " ms");
         return session;
-	}
-
-	@Override
-	public void run(String arg) {
-		try {
-			test();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	// renderSharedFS <urlencode(script)> user[@domain] urlencode(password) url:series,url:series <width> <height> [frames=framerange] [uploadResults=false]
@@ -354,100 +332,6 @@ public class Animation3DClient implements PlugIn {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
-	}
-
-	public static void test() throws UnknownHostException, IOException {
-		String omeroHost = Prefs.get("Animation3DClient.omeroHost", "");
-		String omeroUser = Prefs.get("Animation3DClient.omeroUser", "");
-		String omeroImageId = Prefs.get("Animation3DClient.omeroImageId", "");
-		String processingHost = Prefs.get("Animation3DClient.processingHost", "localhost");
-		int processingPort = Prefs.getInt("Animation3DClient.processingHost", 3333);
-		String animationScript = Prefs.get("Animation3DClient.animationScript", "");
-		int targetWidth = Prefs.getInt("Animation3DClient.targetWidth", 800);
-		int targetHeight = Prefs.getInt("Animation3DClient.targetHeight", 600);
-
-
-		GenericDialogPlus gd = new GenericDialogPlus("Animation3DClient");
-		gd.addStringField("OMERO_Host", omeroHost, 30);
-		gd.addStringField("OMERO_User", omeroUser, 30);
-		gd.setEchoChar('*');
-		gd.addStringField("OMERO_Password", "", 30);
-		gd.addStringField("OMERO_Image_ID", omeroImageId);
-		gd.addNumericField("Target_Width", targetWidth, 0);
-		gd.addNumericField("Target_Height", targetHeight, 0);
-		gd.addStringField("Processing_Host", processingHost + ":" + processingPort, 30);
-		gd.addFileField("Animation_Script", animationScript, 30);
-		gd.showDialog();
-		if(gd.wasCanceled())
-			return;
-
-		omeroHost = gd.getNextString();
-		omeroUser = gd.getNextString();
-		String omeroPass = gd.getNextString();
-		omeroImageId = gd.getNextString();
-		targetWidth = (int)gd.getNextNumber();
-		targetHeight = (int)gd.getNextNumber();
-		String[] tmp = gd.getNextString().split(":");
-		processingHost = tmp[0].trim();
-		processingPort = Integer.parseInt(tmp[1]);
-		animationScript = gd.getNextString();
-
-		Prefs.set("Animation3DClient.omeroHost", omeroHost);
-		Prefs.set("Animation3DClient.omeroUser", omeroUser);
-		Prefs.set("Animation3DClient.omeroImageId", omeroImageId);
-		Prefs.set("Animation3DClient.processingHost", processingHost);
-		Prefs.set("Animation3DClient.processingPort", processingPort);
-		Prefs.set("Animation3DClient.targetWidth", targetWidth);
-		Prefs.set("Animation3DClient.targetHeight", targetHeight);
-		Prefs.set("Animation3DClient.animationScript", animationScript);
-		Prefs.savePreferences();
-
-		String session = omeroLogin(omeroHost, 4064, omeroUser, omeroPass);
-
-		String script = IJ.openAsString(animationScript);
-
-		String[] itoks = omeroImageId.split(",");
-		int[] imageIds = new int[itoks.length];
-		for(int i = 0; i < itoks.length; i++)
-			imageIds[i] = Integer.parseInt(itoks[i].trim());
-
-		String[] basenames = startRendering(
-				omeroHost, session, imageIds, script, null,
-				targetWidth, targetHeight,
-				processingHost, processingPort, null);
-		IJ.log("basenames = " + Arrays.toString(basenames));
-		outer:
-		for(String basename : basenames) {
-			while(true) {
-				String positionProgressState = getState(processingHost, processingPort, basename);
-				String[] toks = positionProgressState.split(" ");
-				int position = Integer.parseInt(toks[0]);
-				double progress = Double.parseDouble(toks[1]);
-				String state = toks[2];
-				if(state.startsWith("ERROR")) {
-					String msg = "An error happened during rendering\n";
-					msg += getStacktrace(processingHost, processingPort, basename);
-					IJ.log(msg);
-					continue outer;
-				}
-				if(state.startsWith("FINISHED")) {
-					IJ.log("Done");
-					break;
-				}
-				IJ.log(state + ": " + progress);
-				try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			File f = downloadAVI(processingHost, processingPort, basename);
-			AVI_Reader reader = new AVI_Reader();
-			reader.setVirtual(false);
-			reader.displayDialog(false);
-			reader.run(f.getAbsolutePath());
-			reader.getImagePlus().show();
 		}
 	}
 }
